184ef2dfc9abedbb9778234c45322a984af5a139
Sunchhay <sunchhay395@gmail.com>
2024-05-04 11:47:46 +0700

    update index endpoint

--- src/modules/app/app.service.ts
+++ src/modules/app/app.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { ENV_MODE } from '../../constants/env-constants';
+import { API_BASE_URL, ENV_MODE } from '../../constants/env-constants';
 
 @Injectable()
 export class AppService {
   constructor(private configService: ConfigService) {}
   index() {
-    return `Capstone API. Env ${this.configService.get(ENV_MODE)}`;
+    return {
+      message: 'Hello! This is Capstone 2 API',
+      env: this.configService.get(ENV_MODE),
+      baseUrl: this.configService.get(API_BASE_URL),
+    };
   }
 }
