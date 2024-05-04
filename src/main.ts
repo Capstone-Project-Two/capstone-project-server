52285a31d2e887367e1348c1cdf149ed75e22620
Sunchhay <sunchhay395@gmail.com>
2024-05-04 14:00:44 +0700

    add global prefix

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { APP_PORT } from './constants/env-constants';
+import { API_BASE_URL, APP_PORT } from './constants/env-constants';
 import { SwaggerModule } from '@nestjs/swagger';
 import { documentBuilder } from './config/swagger/options';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   const configService = app.get(ConfigService);
   const document = SwaggerModule.createDocument(app, documentBuilder);
-  
-  SwaggerModule.setup('api', app, document);
+
+  SwaggerModule.setup('', app, document);
 
   await app.listen(parseInt(configService.get(APP_PORT)));
+
+  console.log(`API is running on: ${configService.get(API_BASE_URL)}`);
 }
 bootstrap();
