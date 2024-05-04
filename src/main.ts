ee41d526decaf5030c82f7825e6cbe8e2aee4d34
Sunchhay <sunchhay395@gmail.com>
2024-05-04 14:10:50 +0700

    add console log for api logs

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { APP_PORT } from './constants/env-constants';
+import { API_BASE_URL, APP_PORT } from './constants/env-constants';
 import { SwaggerModule } from '@nestjs/swagger';
 import { documentBuilder } from './config/swagger/options';
 
@@ -9,9 +9,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const document = SwaggerModule.createDocument(app, documentBuilder);
-  
+
   SwaggerModule.setup('api', app, document);
 
   await app.listen(parseInt(configService.get(APP_PORT)));
+
+  console.log(`Api is running on: ${configService.get(API_BASE_URL)}`);
 }
 bootstrap();
