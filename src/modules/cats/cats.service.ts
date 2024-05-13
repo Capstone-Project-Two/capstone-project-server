dc62f0d6e92351cfe04d76895fd1212eb8578ae3
RUiNe4 <sunchhay@gmail.com>
2024-05-13 15:32:11 +0700

    add post endpoint

--- src/modules/cats/cats.service.ts
+++ src/modules/cats/cats.service.ts
@@ -2,12 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Cat } from 'src/schemas/cat.schema';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { CatResponseDto } from './dto/response/cat-response.dto';
 
 @Injectable()
 export class CatsService {
   constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
 
-  findAll() {
-    return this.catModel.find().exec();
+  async findAll(): Promise<Array<CatResponseDto>> {
+    return await this.catModel.find().exec();
+  }
+
+  async createCat(createCatDto: CreateCatDto) {
+    const res = await this.catModel.create(createCatDto);
+    return res;
   }
 }
