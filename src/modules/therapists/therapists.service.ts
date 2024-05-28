19ee6bf52e5f0a3bce0af50ac185cfc520b9ca95
RethtihpongEM <rethtihpong@gmail.com>
2024-05-28 10:55:10 +0700

    pagination with page number and limi to findAll Therapist

--- src/modules/therapists/therapists.service.ts
+++ src/modules/therapists/therapists.service.ts
@@ -1,10 +1,18 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateTherapistDto } from './dto/create-therapist.dto';
 import { UpdateTherapistDto } from './dto/update-therapist.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Therapist } from 'src/database/schemas/therapist.schema';
 import { phoneFormat } from 'src/utils/helpter';
+import { PaginationParamDto } from 'src/common/dto/pagination-param.dto';
+import { error } from 'console';
+import { getPaginateMeta } from 'src/common/paginate';
 
 @Injectable()
 export class TherapistsService {
@@ -27,8 +35,38 @@ export class TherapistsService {
     }
   }
 
-  async findAll() {
-    return await this.therapistModel.find().exec();
+  async findAll(pagination: PaginationParamDto) {
+    const { limit, page } = pagination;
+    try {
+      const skip = page * limit - limit;
+      const res = await this.therapistModel
+        .find()
+        .limit(limit)
+        .skip(Number(page) === 0 || Number(page) === 1 ? 0 : skip)
+        .exec();
+      if (page > 1 && res.length === 0)
+        throw new HttpException(
+          {
+            status: HttpStatus.NO_CONTENT,
+            error: 'No Data',
+          },
+          HttpStatus.NO_CONTENT,
+          { cause: error },
+        );
+      return {
+        data: res,
+        meta: {
+          ...(await getPaginateMeta({
+            model: this.therapistModel,
+            limit,
+            page,
+            resLength: res.length,
+          })),
+        },
+      };
+    } catch (error) {
+      return error;
+    }
   }
 
   async findOne(id: string) {
