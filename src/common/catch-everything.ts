8d2f27df0dbd3a581ec23db34b7fe27405d7ce08
Sunchhay <sunchhay395@gmail.com>
2024-05-31 20:24:18 +0700

    catch all exception function

---
+++ src/common/catch-everything.ts
@@ -0,0 +1,43 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  catch(exception: Error, host: ArgumentsHost): void {
+    const { httpAdapter } = this.httpAdapterHost;
+
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest();
+    const requestQuery = request.query;
+
+    const httpStatus =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const responseBody = {
+      statusCode: httpStatus,
+      timestamp: new Date().toISOString(),
+      path: httpAdapter.getRequestUrl(ctx.getRequest()),
+      messages: [exception.message ?? String(exception)],
+      errorType: exception.name,
+      validationMessages: Boolean(exception['response']['message'])
+        ? exception['response']['message']
+        : [],
+    };
+
+    httpAdapter.reply(
+      ctx.getResponse(),
+      requestQuery.page <= 0 ? { ...responseBody, data: [] } : responseBody,
+      httpStatus,
+    );
+  }
+}
