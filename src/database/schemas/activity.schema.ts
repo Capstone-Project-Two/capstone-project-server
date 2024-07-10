d0fcf0b159eac23fb96b997d9ef1582325b0c356
RattanakpanhaKong <krattanakpanha@gmail.com>
2024-07-11 00:58:39 +0900

    Updated Schema

--- src/database/schemas/activity.schema.ts
+++ src/database/schemas/activity.schema.ts
@@ -9,19 +9,31 @@ export type ActivityDocument = HydratedDocument<Activity>;
 @Schema({ timestamps: true })
 export class Activity {
   @Prop({ required: true })
-  task: string;
-
-  // @Prop()
-  // time: Date;
-
-  @Prop()
-  location: string;
+  title: string;
 
   @Prop()
   description: string;
 
+  @Prop()
+  type: string;
+
+  @Prop()
+  fitness?: string;
+
+  @Prop()
+  mentalHealth?: string;
+
+  @Prop()
+  socialSkill?: string;
+
+  @Prop()
+  development?: string;
+
+  @Prop()
+  stresslvl?: number;
+
   @Prop({ type: [{ type: TObjectId, ref: MODEL.ActivityImages }] })
-  postImages: ActivityImages[];
+  activityImages: ActivityImages[];
 }
 
 export const ActivitySchema = SchemaFactory.createForClass(Activity);
--- src/modules/activities/dto/response/activity-response.dto.ts
+++ src/modules/activities/dto/response/activity-response.dto.ts
@@ -1,18 +1,32 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsOptional, IsString, MaxLength } from "class-validator";
-import { BaseResponse } from "src/common/base-response.dto";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString, MaxLength } from 'class-validator';
+import { BaseResponse } from 'src/common/base-response.dto';
 
 export class ActivityResponseDto extends BaseResponse {
+  @ApiProperty({ type: String })
+  title: string;
 
-    @ApiProperty({type: String})
-    task: string
+  @ApiProperty({ type: String })
+  description?: string;
 
-    @ApiProperty({type: String})
-    description?: string;
+  @ApiProperty()
+  type: string;
 
-    @ApiProperty({type: String})
-    location: string
-  
-    @ApiProperty({ type: String, isArray: true })
-    activityImages?: string;
+  @ApiProperty()
+  fitness?: string;
+
+  @ApiProperty()
+  mentalHealth?: string;
+
+  @ApiProperty()
+  socialSkill?: string;
+
+  @ApiProperty()
+  development?: string;
+
+  @ApiProperty()
+  stresslvl?: number;
+
+  @ApiProperty({ type: String, isArray: true })
+  activityImages?: string;
 }
