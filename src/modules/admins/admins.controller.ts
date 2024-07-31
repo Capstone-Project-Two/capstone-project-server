568d51a2d8a647b5dd24964c484baa4ebd8e8a46
RattanakpanhaKong <krattanakpanha@gmail.com>
2024-07-31 23:48:00 +0700

    Patient and Admin Auth

--- src/modules/admins/admins.controller.ts
+++ src/modules/admins/admins.controller.ts
@@ -13,12 +13,14 @@ import { CreateAdminDto } from './dto/create-admin.dto';
 import { UpdateAdminDto } from './dto/update-admin.dto';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AdminResponseDto } from './dto/response/admin-response.dto';
+import { Public } from 'src/common/decorator';
 
 @ApiTags('Admin')
 @Controller('admins')
 export class AdminsController {
   constructor(private readonly adminsService: AdminsService) {}
 
+  @Public()
   @Post()
   create(@Body() createAdminDto: CreateAdminDto) {
     return this.adminsService.create(createAdminDto);
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,22 +1,42 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, Res, HttpCode, HttpStatus, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AdminLoginDto } from './dto/admin-login.dto';
 import { Request, Response } from 'express';
 import { GetCurrentUserId, Public } from 'src/common/decorator';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Public()
-  @Post('login')
-  login(@Body() loginDto: AdminLoginDto, @Res({ passthrough: true }) response: Response) {
-    return this.authService.login(loginDto, response);
+  @Post('patient/register')
+  register(@Body() registerDto: RegisterDto, @Res({ passthrough: true }) response: Response) {
+    return this.authService.patient_register(registerDto, response);
   }
 
-  @Get('logout')
+  @Public()
+  @Post('patient/login')
+  patient_login(@Body() loginDto: LoginDto, @Res({ passthrough: true }) response: Response) {
+    return this.authService.patient_login(loginDto, response);
+  }
+
+  @Get('patient/logout')
+  @HttpCode(HttpStatus.OK)
+  patient_logout(@GetCurrentUserId() id: string, @Req() request: Request, @Res({ passthrough: true }) response: Response) {
+    console.log("touch controller")
+    return this.authService.patient_logout(id, request, response);
+  }
+  
+  @Public()
+  @Post('admin/login')
+  login(@Body() loginDto: LoginDto, @Res({ passthrough: true }) response: Response) {
+    return this.authService.admin_login(loginDto, response);
+  }
+
+  @Get('admin/logout')
   @HttpCode(HttpStatus.OK)
   logout(@GetCurrentUserId() id: string, @Req() request: Request, @Res({ passthrough: true }) response: Response) {
-    return this.authService.logout(id, request, response);
+    return this.authService.admin_logout(id, request, response);
   }
 }
