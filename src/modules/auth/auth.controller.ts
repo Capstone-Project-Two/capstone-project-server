f46da2262669bea9e8953c7d2239105959ee2f06
RattanakpanhaKong <krattanakpanha@gmail.com>
2024-08-05 22:08:53 +0700

    ApiTags

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,7 +4,9 @@ import { Request, Response } from 'express';
 import { GetCurrentUserId, Public } from 'src/common/decorator';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
